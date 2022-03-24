2819f82c149067193f0b007a831e125f64d67d5c
GitChann <gitprobata123@gmail.com>
2022-03-24 11:36:05 +0200

    Tuka slojih divisition n shii i da moa si polzvash parola (its abc1234) i nekvi drugi gluposti

--- GitExercise/Startup.cs
+++ GitExercise/Startup.cs
@@ -4,8 +4,28 @@ namespace GitExercise
 {
     public class Startup
     {
+        private static bool CheckCredentials()
+        {
+            Console.Write("Enter passwords to gain access: ");
+            string password = Console.ReadLine();
+            Console.Clear();
+
+            return password == Password;
+        }
+
+        private const string Password = "abc1234";
+
         public static void Main()
         {
+            bool isAuthorized = CheckCredentials();
+
+            if(!isAuthorized)
+            {
+                Console.WriteLine("Access denied.");
+                Console.ReadKey(intercept: true);
+                return;
+            }
+
             Console.WriteLine("Console Calculator App");
             Console.WriteLine(new string('-', 15));
 
@@ -34,6 +54,12 @@ namespace GitExercise
                     break;
                 case "m":
                     OptionsManager.Multiply(a, b);
+                    break;
+                case "d":
+                    OptionsManager.Divide(a, b);
+                    break;
+                case "sabs":
+                    OptionsManager.SubtractAbs(a, b);
                     break;
             }
 
